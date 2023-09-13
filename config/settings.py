0a6e352a9fcb085b79a2f0bc51801b23eba938a5
dozzzini <dozzzini@naver.com>
2023-09-13 18:15:55 +0900

    allowed hosts all

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = get_env_variable("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
